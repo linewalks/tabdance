0ce2cc4249217a947498650096d9c3f7016e9534
Insu Choi <insu@linewalks.com>
2021-12-06 15:46:20 +0900

    feat: add sub arugmentsparser
    - command.py 파일 생성, CommandParser 클래스 생성
    - base.py 파일에서 get_args() 함수 제거
    - 위 파일 수정에 따른 run.py:main() 수정

---
+++ main/command.py
@@ -0,0 +1,32 @@
+import argparse
+
+
+class CommandParser:
+  def __init__(self) -> None:
+    self.parser = argparse.ArgumentParser(prog="TDS")
+    self.subparsers = self.parser.add_subparsers(dest="command", help="Table Data Sync Operation")
+
+  def get_args(self) -> argparse.Namespace:
+    self.add_updownload_command()
+    self.add_update_command()
+    self.add_config_command()
+    return type(self.parser.parse_args())
+
+  def add_updownload_command(self) -> None:
+    for load_type in ["upload", "download"]:
+      if load_type == "upload":
+        help_comment = f"Upload data from LOCAL_REPO_PATH to REMOTE_REPO_PATH"
+      else:
+        help_comment = f"Download data from REMOTE_REPO_PATH to LOCAL_REPO_PATH"
+
+      load_parser = self.subparsers.add_parser(load_type, help=help_comment)
+      options_group = load_parser.add_mutually_exclusive_group()
+      options_group.add_argument("-f", "--file", nargs="+", help=f"Write file name you want to {load_type}")
+      options_group.add_argument("-a", "--all", action="store_true", help=f"All file {load_type}")
+
+  # TODO: help comment 작성
+  def add_update_command(self) -> None:
+    self.subparsers.add_parser("update", help="update help")
+
+  def add_config_command(self) -> None:
+    self.subparsers.add_parser("config", help="config help")
--- main/run.py
+++ main/run.py
@@ -1,25 +1,32 @@
-from main.base import get_args, get_config
+from main.base import get_config
+from main.command import CommandParser
 from main.update import DBTableBase, DBTableSync
 from main.updownload import UpDownLoader
 
 
 def main():
   """main"""
-  args = get_args()
+  args = CommandParser().get_args()
   config = get_config()
 
-  if args.mode == "update":
-    DBTableBase(config).init_db_object()
-    DBTableSync(config).sync_table()
-
-  else:
+  # tds upload or download 실행
+  if args.command == "upload" or args.command == "download":
     updownloader = UpDownLoader(args, config)
     updownloader.connect_sftp()
 
-    if args.mode == "upload":
+    if args.command == "upload":
       updownloader.upload()
-
-    elif args.mode == "download":
+    elif args.command == "download":
       updownloader.download()
 
     updownloader.disconnect_sftp()
+
+  # tds update 실행
+  elif args.command == "update":
+    DBTableBase(config).init_db_object()
+    DBTableSync(config).sync_table()
+
+  # tds config 실행
+  elif args.command == "config":
+    # TODO: config 명령어 실행시 CRUD 기능 개발
+    pass
