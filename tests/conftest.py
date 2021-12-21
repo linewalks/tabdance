f503c649eb569bfb5e4d5f3e55d0173f97f60693
Insu Choi <insu@linewalks.com>
2021-12-21 14:47:38 +0900

    fix(test_upload.py): download test code 작성시 비슷한 부분을 conftest.py 파일로 분리
    [conftest.py]
    - TestFileBase
      - setup_* 관련 메서드 작성
      - 추상 메서드를 나타내는 데코레이터 작성
    
    - UploadTestFile
      - os 를 사용해 csv, meta, td 파일을 생성하고 삭제하는 메서드 작성
    
    [test_upload.py]
    - UploadTestFile Class 를 conftest.py 파일에 작성

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
+import csv
+import json
 import os
 import pytest
+
+from abc import ABCMeta, abstractmethod
 
 from tabdanc.config import TableDataSyncConfig
 
@@ -11,6 +15,8 @@ def delete_config_file_and_directory(test_config):
   if os.path.exists(test_config.tabdanc_directory_path):
     if len(os.listdir(test_config.tabdanc_directory_path)) == 0:
       os.rmdir(test_config.tabdanc_directory_path)
+    else:
+      raise Exception(f"Directory not empty: '{test_config.tabdanc_directory_path}'")
 
 
 @pytest.fixture(scope="session")
@@ -44,3 +50,86 @@ def test_ssh_config(test_config):
   ), "Before execute test file, input test_config info in 'conftest.py'"
 
   return test_ssh_config
+
+
+class TestFileBase(metaclass=ABCMeta):
+  def setup_csv_meta_td_files(self):
+    num = 3
+    self.create_csv_files(num)
+    self.create_meta_files(num)
+    self.create_td_files(num)
+
+  def setup_csv_meta_files(self):
+    num = 1
+    self.create_csv_files(num)
+    self.create_meta_files(num)
+
+  def setup_csv_td_files(self):
+    num = 1
+    self.create_csv_files(num)
+    self.create_td_files(num)
+
+  def setup_meta_td_files(self):
+    num = 1
+    self.create_meta_files(num)
+    self.create_td_files(num)
+
+  @abstractmethod
+  def create_csv_files(self, count):
+    pass
+
+  @abstractmethod
+  def create_meta_files(self, count):
+    pass
+
+  @abstractmethod
+  def create_td_files(self, count):
+    pass
+
+  @abstractmethod
+  def remove_test_files(self):
+    pass
+
+
+class UploadTestFile(TestFileBase):
+  def __init__(self, config):
+    self.local_repo_path = config.get("PATH", "local_repo_path")
+
+  def create_csv_files(self, count):
+    for i in range(count):
+      csv_file = os.path.join(self.local_repo_path, f"tabdanc_test{i}.csv")
+      with open(csv_file, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(["col1", "col2", "col3"])
+
+  def create_meta_files(self, count):
+    for i in range(count):
+      meta_file = os.path.join(self.local_repo_path, f"tabdanc_test{i}.meta")
+      data = {
+          "table_name": f"tabdanc_test_table{i}"
+      }
+      with open(meta_file, "w") as file:
+        json.dump(data, file)
+
+  def create_td_files(self, count):
+    for i in range(count):
+      td_file = os.path.join(self.local_repo_path, f"tabdanc_test_table{i}.td")
+      data = {
+          "columns": [
+              {"name": "seq", "type": "int4"},
+              {"name": "code", "type": "varchar"}
+          ]
+      }
+      with open(td_file, "w") as file:
+        json.dump(data, file)
+
+  def remove_test_files(self):
+    test_files = os.listdir(self.local_repo_path)
+    for file in test_files:
+      file_path = os.path.join(self.local_repo_path, file)
+      if file.endswith(".csv"):
+        os.remove(file_path)
+      elif file.endswith(".meta"):
+        os.remove(file_path)
+      elif file.endswith(".td"):
+        os.remove(file_path)
